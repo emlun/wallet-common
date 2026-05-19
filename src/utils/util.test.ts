50414335483609283cbc2fa25ad3a9d666f2597e
Emil Lundberg <emil@yubico.com>
2026-05-19 09:02:40 +0200

    Add util.test.ts

---
+++ src/utils/util.test.ts
@@ -0,0 +1,22 @@
+import { assert, describe, it } from "vitest";
+
+import { fromHex, toHex } from "../utils/util";
+
+
+describe("fromHex", () => {
+	it("is the inverse of toHex.", async () => {
+		const len = Math.round(128 * Math.random());
+		const data = crypto.getRandomValues(new Uint8Array(len));
+		assert.equal(toHex(data), toHex(fromHex(toHex(data))));
+	});
+
+	it("accepts uppercase and lowercase input.", async () => {
+		const data = fromHex("aABbCc");
+		assert.equal(toHex(data), "aabbcc");
+	});
+
+	it("ignores spaces.", async () => {
+		const data = fromHex("aa bb cc");
+		assert.equal(toHex(data), "aabbcc");
+	});
+});
