037ab2b8d1fc30e50477787a081854abf68afd59
Emil Lundberg <emil@yubico.com>
2026-05-19 09:39:19 +0200

    Wrap long fromHex inputs in bbs.test.ts

--- src/bbs/bbs.test.ts
+++ src/bbs/bbs.test.ts
@@ -47,7 +47,11 @@ describe("Suite:", () => {
 				const PK = SkToPk(SK);
 
 				assert.equal(SK, 0x60e55110f76883a13d030b2f6bd11883422d5abde717569fc0731f51237169fcn);
-				assert.equal(toHex(PK), "a820f230f6ae38503b86c70dc50b61c58a77e45c39ab25c0652bbaa8fa136f2851bd4781c9dcde39fc9d1d52c9e60268061e7d7632171d91aa8d460acee0e96f1e7c4cfb12d3ff9ab5d5dc91c277db75c845d649ef3c4f63aebc364cd55ded0c");
+				assert.equal(
+					toHex(PK),
+					"a820f230f6ae38503b86c70dc50b61c58a77e45c39ab25c0652bbaa8fa136f2851bd4781c9dcde39fc9d1d52c9e60268" +
+					"061e7d7632171d91aa8d460acee0e96f1e7c4cfb12d3ff9ab5d5dc91c277db75c845d649ef3c4f63aebc364cd55ded0c",
+				);
 			});
 		});
 
@@ -135,16 +139,22 @@ describe("Suite:", () => {
 			describe("pass test vectors:", async () => {
 
 				const SK = 0x60e55110f76883a13d030b2f6bd11883422d5abde717569fc0731f51237169fcn;
-				const PK = fromHex("a820f230f6ae38503b86c70dc50b61c58a77e45c39ab25c0652bbaa8fa136f2851bd4781c9dcde39fc9d1d52c9e60268061e7d7632171d91aa8d460acee0e96f1e7c4cfb12d3ff9ab5d5dc91c277db75c845d649ef3c4f63aebc364cd55ded0c");
+				const PK = fromHex(
+					"a820f230f6ae38503b86c70dc50b61c58a77e45c39ab25c0652bbaa8fa136f2851bd4781c9dcde39fc9d1d52c9e60268" +
+					"061e7d7632171d91aa8d460acee0e96f1e7c4cfb12d3ff9ab5d5dc91c277db75c845d649ef3c4f63aebc364cd55ded0c");
 				const header = fromHex("11223344556677889900aabbccddeeff");
-				const signature = fromHex("8339b285a4acd89dec7777c09543a43e3cc60684b0a6f8ab335da4825c96e1463e28f8c5f4fd0641d19cec5920d3a8ff4bedb6c9691454597bbd298288abed3632078557b2ace7d44caed846e1a0a1e8");
+				const signature = fromHex(
+					"8339b285a4acd89dec7777c09543a43e3cc60684b0a6f8ab335da4825c96e1463e28f8c5f4fd0641" +
+					"d19cec5920d3a8ff4bedb6c9691454597bbd298288abed3632078557b2ace7d44caed846e1a0a1e8");
 
 				it("Valid Single Message Signature", async () => {
 					// https://www.ietf.org/archive/id/draft-irtf-cfrg-bbs-signatures-08.html#name-valid-single-message-signatu
 					const { Sign, Verify } = getCipherSuite(suiteId);
 
 					const m_1 = fromHex("9872ad089e452c7b6e283dfac2a80d58e8d0ff71cc4d5e310a1debdda4a45f02");
-					const expectSignature = fromHex("84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0eae615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
+					const expectSignature = fromHex(
+						"84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0ea" +
+						"e615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
 
 					const signature = toU8(await Sign(SK, PK, header, [m_1]));
 
@@ -185,7 +195,9 @@ describe("Suite:", () => {
 					const { Sign, Verify } = getCipherSuite(suiteId);
 
 					const header = fromHex("");
-					const expectSignature = fromHex("8c87e2080859a97299c148427cd2fcf390d24bea850103a9748879039262ecf4f42206f6ef767f298b6a96b424c1e86c26f8fba62212d0e05b95261c2cc0e5fdc63a32731347e810fd12e9c58355aa0d");
+					const expectSignature = fromHex(
+						"8c87e2080859a97299c148427cd2fcf390d24bea850103a9748879039262ecf4f42206f6ef767f29" +
+						"8b6a96b424c1e86c26f8fba62212d0e05b95261c2cc0e5fdc63a32731347e810fd12e9c58355aa0d");
 
 					const signature = toU8(await Sign(SK, PK, header, messages));
 					assert.equal(toHex(signature), toHex(expectSignature));
@@ -199,7 +211,9 @@ describe("Suite:", () => {
 					const { Sign, Verify } = getCipherSuite(suiteId);
 
 					const modifiedMessages = [""].map(fromHex);
-					const signature = fromHex("84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0eae615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
+					const signature = fromHex(
+						"84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0ea" +
+						"e615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
 
 					assert.equal(toHex(await Sign(SK, PK, header, [messages[0]])), toHex(signature));
 					await asyncAssertThrows(() => Verify(PK, signature, header, modifiedMessages), "Expected negative test case to fail signature verification");
@@ -213,7 +227,9 @@ describe("Suite:", () => {
 						"9872ad089e452c7b6e283dfac2a80d58e8d0ff71cc4d5e310a1debdda4a45f02",
 						"c344136d9ab02da4dd5908bbba913ae6f58c2cc844b802a6f811f5fb075f9b80", // Omitted in signature
 					].map(fromHex);
-					const signature = fromHex("84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0eae615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
+					const signature = fromHex(
+						"84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0ea" +
+						"e615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
 
 					assert.equal(toHex(await Sign(SK, PK, header, [messages[0]])), toHex(signature));
 					await asyncAssertThrows(() => Verify(PK, signature, header, modifiedMessages), "Expected negative test case to fail signature verification");
@@ -257,7 +273,9 @@ describe("Suite:", () => {
 				it("Wrong Public Key Signature", async () => {
 					const { Sign, Verify } = getCipherSuite(suiteId);
 
-					const wrongPK = fromHex("b064bd8d1ba99503cbb7f9d7ea00bce877206a85b1750e5583dd9399828a4d20610cb937ea928d90404c239b2835ffb104220a9c66a4c9ed3b54c0cac9ea465d0429556b438ceefb59650ddf67e7a8f103677561b7ef7fe3c3357ec6b94d41c6");
+					const wrongPK = fromHex(
+						"b064bd8d1ba99503cbb7f9d7ea00bce877206a85b1750e5583dd9399828a4d20610cb937ea928d90404c239b2835ffb1" +
+						"04220a9c66a4c9ed3b54c0cac9ea465d0429556b438ceefb59650ddf67e7a8f103677561b7ef7fe3c3357ec6b94d41c6");
 
 					assert.equal(toHex(await Sign(SK, PK, header, messages)), toHex(signature));
 					await asyncAssertThrows(() => Verify(wrongPK, signature, header, messages), "Expected negative test case to fail signature verification");
@@ -291,17 +309,30 @@ describe("Suite:", () => {
 					},
 				);
 
-				const public_key = fromHex("a820f230f6ae38503b86c70dc50b61c58a77e45c39ab25c0652bbaa8fa136f2851bd4781c9dcde39fc9d1d52c9e60268061e7d7632171d91aa8d460acee0e96f1e7c4cfb12d3ff9ab5d5dc91c277db75c845d649ef3c4f63aebc364cd55ded0c");
+				const public_key = fromHex(
+					"a820f230f6ae38503b86c70dc50b61c58a77e45c39ab25c0652bbaa8fa136f2851bd4781c9dcde39fc9d1d52c9e60268" +
+					"061e7d7632171d91aa8d460acee0e96f1e7c4cfb12d3ff9ab5d5dc91c277db75c845d649ef3c4f63aebc364cd55ded0c");
 				const header = fromHex("11223344556677889900aabbccddeeff");
 				const presentation_header = fromHex("bed231d880675ed101ead304512e043ade9958dd0241ea70b4b3957fba941501");
-				const signature = fromHex("8339b285a4acd89dec7777c09543a43e3cc60684b0a6f8ab335da4825c96e1463e28f8c5f4fd0641d19cec5920d3a8ff4bedb6c9691454597bbd298288abed3632078557b2ace7d44caed846e1a0a1e8");
+				const signature = fromHex(
+					"8339b285a4acd89dec7777c09543a43e3cc60684b0a6f8ab335da4825c96e1463e28f8c5f4fd0641" +
+					"d19cec5920d3a8ff4bedb6c9691454597bbd298288abed3632078557b2ace7d44caed846e1a0a1e8");
 
 				// https://www.ietf.org/archive/id/draft-irtf-cfrg-bbs-signatures-08.html#name-valid-single-message-proof-2
 				it("Valid Single Message Proof", async () => {
 					const m_0 = messages[0];
-					const signature = fromHex("84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0eae615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
+					const signature = fromHex(
+						"84773160b824e194073a57493dac1a20b667af70cd2352d8af241c77658da5253aa8458317cca0e" +
+						"ae615690d55b1f27164657dcafee1d5c1973947aa70e2cfbb4c892340be5969920d0916067b4565a0");
 					const revealed_indexes = [0];
-					const expectProof = fromHex("94916292a7a6bade28456c601d3af33fcf39278d6594b467e128a3f83686a104ef2b2fcf72df0215eeaf69262ffe8194a19fab31a82ddbe06908985abc4c9825788b8a1610942d12b7f5debbea8985296361206dbace7af0cc834c80f33e0aadaeea5597befbb651827b5eed5a66f1a959bb46cfd5ca1a817a14475960f69b32c54db7587b5ee3ab665fbd37b506830a49f21d592f5e634f47cee05a025a2f8f94e73a6c15f02301d1178a92873b6e8634bafe4983c3e15a663d64080678dbf29417519b78af042be2b3e1c4d08b8d520ffab008cbaaca5671a15b22c239b38e940cfeaa5e72104576a9ec4a6fad78c532381aeaa6fb56409cef56ee5c140d455feeb04426193c57086c9b6d397d9418");
+					const expectProof = fromHex(
+						"94916292a7a6bade28456c601d3af33fcf39278d6594b467e128a3f83686a104ef2b2fcf72df0215eeaf69262ffe8194" +
+						"a19fab31a82ddbe06908985abc4c9825788b8a1610942d12b7f5debbea8985296361206dbace7af0cc834c80f33e0aad" +
+						"aeea5597befbb651827b5eed5a66f1a959bb46cfd5ca1a817a14475960f69b32c54db7587b5ee3ab665fbd37b506830a" +
+						"49f21d592f5e634f47cee05a025a2f8f94e73a6c15f02301d1178a92873b6e86" +
+						"34bafe4983c3e15a663d64080678dbf29417519b78af042be2b3e1c4d08b8d52" +
+						"0ffab008cbaaca5671a15b22c239b38e940cfeaa5e72104576a9ec4a6fad78c5" +
+						"32381aeaa6fb56409cef56ee5c140d455feeb04426193c57086c9b6d397d9418");
 
 					const proof = await ProofGen(public_key, signature, header, presentation_header, [m_0], revealed_indexes);
 					assert.equal(toHex(proof), toHex(expectProof));
@@ -315,7 +346,14 @@ describe("Suite:", () => {
 				// https://www.ietf.org/archive/id/draft-irtf-cfrg-bbs-signatures-08.html#name-valid-multi-message-all-mess
 				it("Valid Multi-Message, All Messages Disclosed Proof", async () => {
 					const revealed_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-					const expectProof = fromHex("b1f468aec2001c4f54cb56f707c6222a43e5803a25b2253e67b2210ab2ef9eab52db2d4b379935c4823281eaf767fd37b08ce80dc65de8f9769d27099ae649ad4c9b4bd2cc23edcba52073a298087d2495e6d57aaae051ef741adf1cbce65c64a73c8c97264177a76c4a03341956d2ae45ed3438ce598d5cda4f1bf9507fecef47855480b7b30b5e4052c92a4360110c67327365763f5aa9fb85ddcbc2975449b8c03db1216ca66b310f07d0ccf12ab460cdc6003b677fed36d0a23d0818a9d4d098d44f749e91008cf50e8567ef936704c8277b7710f41ab7e6e16408ab520edc290f9801349aee7b7b4e318e6a76e028e1dea911e2e7baec6a6a174da1a22362717fbae1cd961d7bf4adce1d31c2ab");
+					const expectProof = fromHex(
+						"b1f468aec2001c4f54cb56f707c6222a43e5803a25b2253e67b2210ab2ef9eab52db2d4b379935c4823281eaf767fd37" +
+						"b08ce80dc65de8f9769d27099ae649ad4c9b4bd2cc23edcba52073a298087d2495e6d57aaae051ef741adf1cbce65c64" +
+						"a73c8c97264177a76c4a03341956d2ae45ed3438ce598d5cda4f1bf9507fecef47855480b7b30b5e4052c92a4360110c" +
+						"67327365763f5aa9fb85ddcbc2975449b8c03db1216ca66b310f07d0ccf12ab4" +
+						"60cdc6003b677fed36d0a23d0818a9d4d098d44f749e91008cf50e8567ef9367" +
+						"04c8277b7710f41ab7e6e16408ab520edc290f9801349aee7b7b4e318e6a76e0" +
+						"28e1dea911e2e7baec6a6a174da1a22362717fbae1cd961d7bf4adce1d31c2ab");
 
 					const proof = await ProofGen(public_key, signature, header, presentation_header, messages, revealed_indexes);
 					assert.equal(toHex(proof), toHex(expectProof));
@@ -332,7 +370,20 @@ describe("Suite:", () => {
 				// https://www.ietf.org/archive/id/draft-irtf-cfrg-bbs-signatures-08.html#name-valid-multi-message-some-mes
 				it("Valid Multi-Message, Some Messages Disclosed Proof", async () => {
 					const revealed_indexes = [0, 2, 4, 6];
-					const expectProof = fromHex("a2ed608e8e12ed21abc2bf154e462d744a367c7f1f969bdbf784a2a134c7db2d340394223a5397a3011b1c340ebc415199462ba6f31106d8a6da8b513b37a47afe93c9b3474d0d7a354b2edc1b88818b063332df774c141f7a07c48fe50d452f897739228c88afc797916dca01e8f03bd9c5375c7a7c59996e514bb952a436afd24457658acbaba5ddac2e693ac481356918cd38025d86b28650e909defe9604a7259f44386b861608be742af7775a2e71a6070e5836f5f54dc43c60096834a5b6da295bf8f081f72b7cdf7f3b4347fb3ff19edaa9e74055c8ba46dbcb7594fb2b06633bb5324192eb9be91be0d33e453b4d3127459de59a5e2193c900816f049a02cb9127dac894418105fa1641d5a206ec9c42177af9316f433417441478276ca0303da8f941bf2e0222a43251cf5c2bf6eac1961890aa740534e519c1767e1223392a3a286b0f4d91f7f25217a7862b8fcc1810cdcfddde2a01c80fcc90b632585fec12dc4ae8fea1918e9ddeb9414623a457e88f53f545841f9d5dcb1f8e160d1560770aa79d65e2eca8edeaecb73fb7e995608b820c4a64de6313a370ba05dc25ed7c1d185192084963652f2870341bdaa4b1a37f8c06348f38a4f80c5a2650a21d59f09e8305dcd3fc3ac30e2a");
+					const expectProof = fromHex(
+						"a2ed608e8e12ed21abc2bf154e462d744a367c7f1f969bdbf784a2a134c7db2d340394223a5397a3011b1c340ebc4151" +
+						"99462ba6f31106d8a6da8b513b37a47afe93c9b3474d0d7a354b2edc1b88818b063332df774c141f7a07c48fe50d452f" +
+						"897739228c88afc797916dca01e8f03bd9c5375c7a7c59996e514bb952a436afd24457658acbaba5ddac2e693ac48135" +
+						"6918cd38025d86b28650e909defe9604a7259f44386b861608be742af7775a2e" +
+						"71a6070e5836f5f54dc43c60096834a5b6da295bf8f081f72b7cdf7f3b4347fb" +
+						"3ff19edaa9e74055c8ba46dbcb7594fb2b06633bb5324192eb9be91be0d33e45" +
+						"3b4d3127459de59a5e2193c900816f049a02cb9127dac894418105fa1641d5a2" +
+						"06ec9c42177af9316f433417441478276ca0303da8f941bf2e0222a43251cf5c" +
+						"2bf6eac1961890aa740534e519c1767e1223392a3a286b0f4d91f7f25217a786" +
+						"2b8fcc1810cdcfddde2a01c80fcc90b632585fec12dc4ae8fea1918e9ddeb941" +
+						"4623a457e88f53f545841f9d5dcb1f8e160d1560770aa79d65e2eca8edeaecb7" +
+						"3fb7e995608b820c4a64de6313a370ba05dc25ed7c1d185192084963652f2870" +
+						"341bdaa4b1a37f8c06348f38a4f80c5a2650a21d59f09e8305dcd3fc3ac30e2a");
 
 					const proof = await ProofGen(public_key, signature, header, presentation_header, messages, revealed_indexes);
 					assert.equal(toHex(proof), toHex(expectProof));
@@ -351,8 +402,23 @@ describe("Suite:", () => {
 				it("No Header Valid Proof", async () => {
 					const revealed_indexes = [0, 2, 4, 6];
 					const header = fromHex("");
-					const signature = fromHex("8c87e2080859a97299c148427cd2fcf390d24bea850103a9748879039262ecf4f42206f6ef767f298b6a96b424c1e86c26f8fba62212d0e05b95261c2cc0e5fdc63a32731347e810fd12e9c58355aa0d");
-					const expectProof = fromHex("81925c2e525d9fbb0ba95b438b5a13fff5874c7c0515c193628d7d143ddc3bb487771ad73658895997a88dd5b254ed29abc019bfca62c09b8dafb37e5f09b1d380e084ec3623d071ec38d6b8602af93aa0ddbada307c9309cca86be16db53dc7ac310574f509c712bb1a181d64ea3c1ee075c018a2bc773e2480b5c033ccb9bfea5af347a88ab83746c9342ba76db3675ff70ce9006d166fd813a81b448a632216521c864594f3f92965974914992f8d1845230915b11680cf44b25886c5670904ac2d88255c8c31aea7b072e9c4eb7e4c3fdd38836ae9d2e9fa271c8d9fd42f669a9938aeeba9d8ae613bf11f489ce947616f5cbaee95511dfaa5c73d85e4ddd2f29340f821dc2fb40db3eae5f5bc08467eb195e38d7d436b63e556ea653168282a23b53d5792a107f85b1203f82aab46f6940650760e5b320261ffc0ca5f15917b51e7d2ad4bcbec94de792e229db663abff23af392a5e73ce115c27e8492ec24a0815091c69874dbd9dae2d2eed000810c748a798a78a804a39034c6e745cee455812cc982eea7105948b2cb55b82278a77237fcbec4748e2d2255af0994dd09dba8ac60515a39b24632a2c1c840c4a70506add5b2eb0be9ff66e3ea8deae666f198edfbb1391c6834e6df4f1026d");
+					const signature = fromHex(
+						"8c87e2080859a97299c148427cd2fcf390d24bea850103a9748879039262ecf4f42206f6ef767f29" +
+						"8b6a96b424c1e86c26f8fba62212d0e05b95261c2cc0e5fdc63a32731347e810fd12e9c58355aa0d");
+					const expectProof = fromHex(
+						"81925c2e525d9fbb0ba95b438b5a13fff5874c7c0515c193628d7d143ddc3bb487771ad73658895997a88dd5b254ed29" +
+						"abc019bfca62c09b8dafb37e5f09b1d380e084ec3623d071ec38d6b8602af93aa0ddbada307c9309cca86be16db53dc7" +
+						"ac310574f509c712bb1a181d64ea3c1ee075c018a2bc773e2480b5c033ccb9bfea5af347a88ab83746c9342ba76db367" +
+						"5ff70ce9006d166fd813a81b448a632216521c864594f3f92965974914992f8d" +
+						"1845230915b11680cf44b25886c5670904ac2d88255c8c31aea7b072e9c4eb7e" +
+						"4c3fdd38836ae9d2e9fa271c8d9fd42f669a9938aeeba9d8ae613bf11f489ce9" +
+						"47616f5cbaee95511dfaa5c73d85e4ddd2f29340f821dc2fb40db3eae5f5bc08" +
+						"467eb195e38d7d436b63e556ea653168282a23b53d5792a107f85b1203f82aab" +
+						"46f6940650760e5b320261ffc0ca5f15917b51e7d2ad4bcbec94de792e229db6" +
+						"63abff23af392a5e73ce115c27e8492ec24a0815091c69874dbd9dae2d2eed00" +
+						"0810c748a798a78a804a39034c6e745cee455812cc982eea7105948b2cb55b82" +
+						"278a77237fcbec4748e2d2255af0994dd09dba8ac60515a39b24632a2c1c840c" +
+						"4a70506add5b2eb0be9ff66e3ea8deae666f198edfbb1391c6834e6df4f1026d");
 
 					const revealed_messages = [messages[0], messages[2], messages[4], messages[6]];
 					const proof = await ProofGen(public_key, signature, header, presentation_header, messages, revealed_indexes);
@@ -364,8 +430,23 @@ describe("Suite:", () => {
 				it("No Presentation Header Valid Proof", async () => {
 					const revealed_indexes = [0, 2, 4, 6];
 					const presentation_header = fromHex("");
-					const signature = fromHex("8339b285a4acd89dec7777c09543a43e3cc60684b0a6f8ab335da4825c96e1463e28f8c5f4fd0641d19cec5920d3a8ff4bedb6c9691454597bbd298288abed3632078557b2ace7d44caed846e1a0a1e8");
-					const expectProof = fromHex("a2ed608e8e12ed21abc2bf154e462d744a367c7f1f969bdbf784a2a134c7db2d340394223a5397a3011b1c340ebc415199462ba6f31106d8a6da8b513b37a47afe93c9b3474d0d7a354b2edc1b88818b063332df774c141f7a07c48fe50d452f897739228c88afc797916dca01e8f03bd9c5375c7a7c59996e514bb952a436afd24457658acbaba5ddac2e693ac48135672556358e78b5398f1a547a2a98dfe16230f244ba742dea737e4f810b4d94e03ac068ef840aaadf12b2ed51d3fb774c2a0a620019fd1f39c52c6f89a0e6067e3039413a91129791b2af215a82ad2356b6bc305c1d7a828fe519619dd026eaaf07ea81cee52b21aab3e8320519bf37c2bb228a8b580f899d84327bdc5e84a66000e8bac17d2fa039bb2246c8eacc623ccd9eb26e184a96a9e3a6702e1dbafe194772394b05251f72bcd2d20f542b15b2406f899791f6f285c7b469e7c7b9624147f305c38c903273a949f6e85b9774aeeccfafa432e2cdd7c8f97d1687741ed30d725444428dd87d9884711d9a46baaf0c04b03a2a228b7033be0841880134b03b15f698756eca5f37503a0411a9586d3027a8b8b9118e95a9949b2719e85e4a669d9e4b7bb6d4544c8cc558c30d79f9c85a87e1a95611400b7c7dac5673d800");
+					const signature = fromHex(
+						"8339b285a4acd89dec7777c09543a43e3cc60684b0a6f8ab335da4825c96e1463e28f8c5f4fd0641" +
+						"d19cec5920d3a8ff4bedb6c9691454597bbd298288abed3632078557b2ace7d44caed846e1a0a1e8");
+					const expectProof = fromHex(
+						"a2ed608e8e12ed21abc2bf154e462d744a367c7f1f969bdbf784a2a134c7db2d340394223a5397a3011b1c340ebc4151" +
+						"99462ba6f31106d8a6da8b513b37a47afe93c9b3474d0d7a354b2edc1b88818b063332df774c141f7a07c48fe50d452f" +
+						"897739228c88afc797916dca01e8f03bd9c5375c7a7c59996e514bb952a436afd24457658acbaba5ddac2e693ac48135" +
+						"672556358e78b5398f1a547a2a98dfe16230f244ba742dea737e4f810b4d94e0" +
+						"3ac068ef840aaadf12b2ed51d3fb774c2a0a620019fd1f39c52c6f89a0e6067e" +
+						"3039413a91129791b2af215a82ad2356b6bc305c1d7a828fe519619dd026eaaf" +
+						"07ea81cee52b21aab3e8320519bf37c2bb228a8b580f899d84327bdc5e84a660" +
+						"00e8bac17d2fa039bb2246c8eacc623ccd9eb26e184a96a9e3a6702e1dbafe19" +
+						"4772394b05251f72bcd2d20f542b15b2406f899791f6f285c7b469e7c7b96241" +
+						"47f305c38c903273a949f6e85b9774aeeccfafa432e2cdd7c8f97d1687741ed3" +
+						"0d725444428dd87d9884711d9a46baaf0c04b03a2a228b7033be0841880134b0" +
+						"3b15f698756eca5f37503a0411a9586d3027a8b8b9118e95a9949b2719e85e4a" +
+						"669d9e4b7bb6d4544c8cc558c30d79f9c85a87e1a95611400b7c7dac5673d800");
 
 					const revealed_messages = [messages[0], messages[2], messages[4], messages[6]];
 					const proof = await ProofGen(public_key, signature, header, presentation_header, messages, revealed_indexes);
